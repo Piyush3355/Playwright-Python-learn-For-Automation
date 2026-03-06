b0331d2934fc483a3974db6c393bf4b76c6e120e
Piyush3355 <ppatil33555@gmail.com>
2026-03-06 14:59:22 +0530

    day4

---
+++ 06_pytest__organization.py
@@ -0,0 +1,28 @@
+import pytest
+from playwright.sync_api import sync_playwright
+
+@ pytest.fixture
+def login_page():
+    with sync_playwright() as p:
+        browser = p.firefox.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://practicetestautomation.com/practice-test-login/")
+        yield page
+        browser.close()
+
+def test_valid_login(login_page):
+    login_page.fill("#username", "student")
+    login_page.fill("#password", "Password123")
+    login_page.click("#submit")
+    login_page.wait_for_selector("text=Logged In Successfully")
+    content = login_page.content()
+    assert "Congratulations" in content
+
+def test_invalid_login(login_page):
+    login_page.fill("#username", "student")
+    login_page.fill("#password", "Password123")
+    login_page.click("#submit")
+    login_page.wait_for_timeout(1500)
+    content = login_page.content()
+    assert "error" in content.lower()
+        
